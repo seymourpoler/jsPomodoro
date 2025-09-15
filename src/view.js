32d3fc53f32ff7a9c68c8b6f9e64408fa9ae0ac8
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 13:56:05 +0200

    fix: show seconds instead of minutes

--- src/view.js
+++ src/view.js
@@ -33,7 +33,7 @@ function View(){
             return;
         }
         document.getElementById('minutes').innerHTML = time.getMinutes().toString().padStart(2, '0');
-        document.getElementById('seconds').innerHTML = time.getMinutes().toString().padStart(2, '0');
+        document.getElementById('seconds').innerHTML = time.getSeconds().toString().padStart(2, '0');
     }
 }
 
