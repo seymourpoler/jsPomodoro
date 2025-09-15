777b60fb5a19bf74ef7ed25a7d7c801c2176e6df
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 18:40:02 +0200

    add Configuration Presenter unit test

--- src/configuration/ConfigurationPresenter.test.js
+++ src/configuration/ConfigurationPresenter.test.js
@@ -1,3 +1,12 @@
-describe('ConfigurationPresenter', () => {
+import {describe, it} from "vitest";
 
+const ConfigurationView = require("./configurationView");
+const ConfigurationPreseter = require("./configurationPresenter");
+
+describe('ConfigurationPresenter', () => {
+    describe('whe the apply changes is requested', () => {
+        it('applies the changes', () =>{
+
+        });
+    });
 });
