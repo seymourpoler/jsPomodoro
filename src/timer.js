b795a7705ec610fec02b4b68b782778d55819e0b
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 16:37:20 +0200

    feat: avoid a weird behaviour with setInterval

--- src/timer.js
+++ src/timer.js
@@ -1,20 +1,28 @@
 function Timer(){
-    let self = this;
-    let interval= null;
     const oneSecond = 1000;
 
+    let self = this;
+    let interval= null;
+    let isRunning = false;
+
     self.start = function (handler) {
+        if(isRunning){
+            return;
+        }
         interval = setInterval(handler, oneSecond);
+        isRunning = true;
     };
 
     self.stop = function (){
         clearInterval(interval);
         interval = null;
+        isRunning = false;
     };
 
     self.reset = function(){
         clearInterval(interval);
         interval = null;
+        isRunning = false;
     };
 }
 
