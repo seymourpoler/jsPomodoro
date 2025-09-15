c2cf798d90f95fde33ad916dc879dd4b513db864
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 18:58:26 +0200

    add bus

---
+++ src/bus.js
@@ -0,0 +1,15 @@
+function Bus(){
+    let self = this;
+
+    self.subscribe = function(){
+
+    };
+
+    self.publish = function(){
+
+    };
+}
+
+if(module && module.exports){
+    module.exports	= ConfigurationView;
+}
