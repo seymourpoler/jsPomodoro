00933e531f2c3465cb663abcdff1bca08f9732c7
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 18:40:42 +0200

    fix typo

--- src/configuration/ConfigurationPresenter.test.js
+++ src/configuration/ConfigurationPresenter.test.js
@@ -1,7 +1,7 @@
 import {describe, it} from "vitest";
 
 const ConfigurationView = require("./configurationView");
-const ConfigurationPreseter = require("./configurationPresenter");
+const ConfigurationPresenter = require("./configurationPresenter");
 
 describe('ConfigurationPresenter', () => {
     describe('whe the apply changes is requested', () => {
