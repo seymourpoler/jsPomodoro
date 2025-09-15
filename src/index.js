e47a9d988378ed352007e0b8ffff88518b550528
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-15 17:56:26 +0200

    reset to 25 minutes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const view = new View();
 const timer = new Timer();
 const sound = new Sound();
-const time = new Time(0,2);
+const time = new Time(25,0);
 new Presenter(view, timer, sound, time);
 
 console.log('it works');
