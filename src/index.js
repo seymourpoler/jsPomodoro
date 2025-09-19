88ed085b3920d3f46e01b44baba73da76df1867f
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-19 11:21:08 +0200

    add `Configuration`

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ jsPomodoro.version = "0.0.1";
 jsPomodoro.Timer = {};
 jsPomodoro.Configuration = {}
 
+const bus = new Bus();
+
+const configurationView = new ConfigurationView();
+new ConfigurationPresenter(configurationView, bus);
+
 const timerView = new TimerView();
 const timer = new Timer();
 const sound = new Sound();
 const time = new Time(25,0);
-new TimerPresenter(timerView, timer, sound, time);
+new TimerPresenter(timerView, bus, timer, sound, time);
 
 console.log('it works');
