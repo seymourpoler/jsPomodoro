15cb5d7169165d7b02947712f0b8b1ee3d30e514
Luis Rodríguez <luis.rodriguez@leanmind.es>
2025-09-26 10:43:48 +0200

    refactor: format code

--- src/quickPreset/quickPresetPresenter.js
+++ src/quickPreset/quickPresetPresenter.js
@@ -1,7 +1,9 @@
 function QuickPresetPresenter(view, bus) {
+
     view.subscribeToOn25MinutesClicked(() =>{
         bus.publish('selectedQuickPreset', {minutes: 25, seconds: 0});
     });
+
     view.subscribeToOn5MinutesClicked(()=>{
         bus.publish('selectedQuickPreset', {minutes: 5, seconds: 0});
     });
