66be0e67a8077c69ac2a247a7fb449e8de6606ce
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-01-10 11:03:14 +0100

    setup  with 25 minutes and 00 seconds

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,15 +6,15 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
-let numberOfSeconds = 0;
+let numberOfSeconds = 1500;
 let timerInterval: NodeJS.Timeout | undefined = undefined;
 
+const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
         methods: ["GET", "POST"],
-    },
+    }
 });
 
 io.on("connection", (socket) => {
@@ -29,8 +29,7 @@ io.on("connection", (socket) => {
         }
 
         timerInterval = setInterval(() => {
-            numberOfSeconds++;
-            // Broadcast the new numberOfSeconds value to EVERYONE
+            numberOfSeconds--;
             io.emit("updated_time", numberOfSeconds);
         }, 1000);
     });
@@ -44,13 +43,13 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on('reset', (sender) => {
+    socket.on('reset', () => {
         console.log(`User reset: ${socket.id}`);
         if (timerInterval) {
             clearInterval(timerInterval);
             timerInterval = undefined;
         }
-        numberOfSeconds = 0;
+        numberOfSeconds = 1500;
         io.emit("updated_time", numberOfSeconds);
     });
 });
